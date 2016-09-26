26f0fc6edaf3b28d43f7aa408bacb6dcb5f91ccd
Siarhei Kvachonak <segrey1989@tut.by>
2016-09-26 13:01:10 +0300

    Initial Commit

---
+++ StampCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StampCollector
+//
+//  Created by Siarhei Kvachonak on 26.09.16.
+//  Copyright © 2016 Siarhei Kvachonak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
